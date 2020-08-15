a25e146184e1a42550d355f6f3542e513d444257
luke ichpekov <luke.ichepkov@gmail.com>
2020-08-14 22:42:41 -0400

    Updates

---
+++ precache-manifest.b409e9d66d2cee721eb40a62d1fd6525.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448b3949c6e9486057c3d215ba17d2c9",
+    "url": "/react-pokemon-site/index.html"
+  },
+  {
+    "revision": "962e92129b8a494aa340",
+    "url": "/react-pokemon-site/static/css/main.dc98eedb.chunk.css"
+  },
+  {
+    "revision": "6c139f4bebc7bd257003",
+    "url": "/react-pokemon-site/static/js/2.8f537209.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokemon-site/static/js/2.8f537209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962e92129b8a494aa340",
+    "url": "/react-pokemon-site/static/js/main.e4cc6675.chunk.js"
+  },
+  {
+    "revision": "aacb09bb567338cc1919",
+    "url": "/react-pokemon-site/static/js/runtime-main.2aa1c8a9.js"
+  },
+  {
+    "revision": "feaae45dc2f1b4f8935c22d991e55256",
+    "url": "/react-pokemon-site/static/media/mountain.feaae45d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/deploy-pokeSite/precache-manifest.83cef85ba2b217ee70ce2a4e339c8db2.js"
+  "/react-pokemon-site/precache-manifest.b409e9d66d2cee721eb40a62d1fd6525.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deploy-pokeSite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-pokemon-site/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
