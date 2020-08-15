49648e94502c9c5e26219ffed5d994078a4e5506
luke ichpekov <luke.ichepkov@gmail.com>
2020-08-14 21:27:37 -0400

    Updates

---
+++ precache-manifest.8583f4db33c7c209d6b4c5d39993e552.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4be47014c4c8ae1a7dadc7d496ed20d",
+    "url": "/deploy-pokeSite.git/index.html"
+  },
+  {
+    "revision": "cb7e7025c89d7441f83a",
+    "url": "/deploy-pokeSite.git/static/css/main.170b2107.chunk.css"
+  },
+  {
+    "revision": "6c139f4bebc7bd257003",
+    "url": "/deploy-pokeSite.git/static/js/2.8f537209.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deploy-pokeSite.git/static/js/2.8f537209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7e7025c89d7441f83a",
+    "url": "/deploy-pokeSite.git/static/js/main.e4cc6675.chunk.js"
+  },
+  {
+    "revision": "7be70500bbc6a46eb8e0",
+    "url": "/deploy-pokeSite.git/static/js/runtime-main.5fb2dc1a.js"
+  },
+  {
+    "revision": "feaae45dc2f1b4f8935c22d991e55256",
+    "url": "/deploy-pokeSite.git/static/media/mountain.feaae45d.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/deploy-pokeSite.git/precache-manifest.8583f4db33c7c209d6b4c5d39993e552.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deploy-pokeSite.git/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
